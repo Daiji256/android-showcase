206180a336ae60ae4ec54b8a151fd81cb0c57091
Daiji256 <daiji.szk@gmail.com>
2025-02-09 00:00:58 +0900

    add MainContent

---
+++ app/src/main/kotlin/io/github/daiji256/showcase/MainContent.kt
@@ -0,0 +1,25 @@
+package io.github.daiji256.showcase
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import io.github.daiji256.showcase.core.designsystem.theme.ShowcaseTheme
+import io.github.daiji256.showcase.core.ui.urihandler.SafeUriHandler
+import io.github.daiji256.showcase.feature.ktlint.KtlintScreen
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val uriHandler = remember(context) { SafeUriHandler(context) }
+
+    CompositionLocalProvider(
+        LocalUriHandler provides uriHandler,
+    ) {
+        ShowcaseTheme {
+            KtlintScreen(modifier = modifier)
+        }
+    }
+}
