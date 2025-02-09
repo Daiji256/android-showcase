9338308f9195cec150c21621d73156c2f34f576a
Daiji256 <daiji.szk@gmail.com>
2025-02-09 18:13:10 +0900

    sort plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.ktlintGradle) apply false
 }
 
