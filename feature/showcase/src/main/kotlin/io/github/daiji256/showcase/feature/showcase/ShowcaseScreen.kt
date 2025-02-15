5182ef0ae2d711e701cfc2e388731c872b3cf2dd
Daiji256 <daiji.szk@gmail.com>
2025-02-15 19:53:38 +0900

    add sample list

--- feature/showcase/src/main/kotlin/io/github/daiji256/showcase/feature/showcase/ShowcaseScreen.kt
+++ feature/showcase/src/main/kotlin/io/github/daiji256/showcase/feature/showcase/ShowcaseScreen.kt
@@ -1,16 +1,82 @@
 package io.github.daiji256.showcase.feature.showcase
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import io.github.daiji256.showcase.core.ui.document.Document
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun ShowcaseScreen() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.feature_showcase_title),
+                    )
+                },
+            )
+        },
+    ) { padding ->
+        val layoutDirection = LocalLayoutDirection.current
+        LazyColumn(
+            contentPadding = PaddingValues(
+                bottom = padding.calculateBottomPadding(),
+            ),
+            modifier = Modifier
+                .padding(
+                    top = padding.calculateTopPadding(),
+                    start = padding.calculateStartPadding(layoutDirection),
+                    end = padding.calculateEndPadding(layoutDirection),
+                ),
+        ) {
+            items(
+                items = Feature.entries,
+                key = { it },
+            ) { feature ->
+                FeatureItem(
+                    label = feature.name, // TODO
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+private enum class Feature {
+    Ktlint,
+    Roborazzi,
+}
 
 @Composable
-fun ShowcaseScreen() {
-    Document(
-        title = stringResource(id = R.string.feature_showcase_title),
-        markdown = "TODO",
-        onNavigateUpClick = { /*TODO*/ },
+private fun FeatureItem(
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = label,
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(12.dp),
     )
 }
 
