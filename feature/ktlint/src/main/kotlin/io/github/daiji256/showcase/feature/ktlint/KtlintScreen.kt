87be4d6faead32d22191d630f12bedfeb5a73f9c
Daiji256 <daiji.szk@gmail.com>
2025-02-16 16:05:02 +0900

    manual onNavigateUpClick

--- feature/ktlint/src/main/kotlin/io/github/daiji256/showcase/feature/ktlint/KtlintScreen.kt
+++ feature/ktlint/src/main/kotlin/io/github/daiji256/showcase/feature/ktlint/KtlintScreen.kt
@@ -3,13 +3,26 @@ package io.github.daiji256.showcase.feature.ktlint
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.dropUnlessResumed
 import io.github.daiji256.showcase.core.ui.document.Document
+import io.github.daiji256.showcase.core.ui.navigation.LocalNavController
 
 @Composable
 internal fun KtlintScreen() {
+    val navController = LocalNavController.current
+    KtlintScreen(
+        onNavigateUpClick = dropUnlessResumed(block = navController::navigateUp),
+    )
+}
+
+@Composable
+private fun KtlintScreen(
+    onNavigateUpClick: () -> Unit,
+) {
     Document(
         title = stringResource(id = R.string.feature_ktlint_title),
         markdown = stringResource(id = R.string.feature_ktlint_document_md),
+        onNavigateUpClick = onNavigateUpClick,
     )
 }
 
@@ -17,5 +30,7 @@ internal fun KtlintScreen() {
 @Composable
 private fun KtlintScreenPreview() {
     // TODO: Theme
-    KtlintScreen()
+    KtlintScreen(
+        onNavigateUpClick = {},
+    )
 }
--- feature/roborazzi/src/main/kotlin/io/github/daiji256/showcase/feature/roborazzi/RoborazziScreen.kt
+++ feature/roborazzi/src/main/kotlin/io/github/daiji256/showcase/feature/roborazzi/RoborazziScreen.kt
@@ -3,13 +3,26 @@ package io.github.daiji256.showcase.feature.roborazzi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.dropUnlessResumed
 import io.github.daiji256.showcase.core.ui.document.Document
+import io.github.daiji256.showcase.core.ui.navigation.LocalNavController
 
 @Composable
 internal fun RoborazziScreen() {
+    val navController = LocalNavController.current
+    RoborazziScreen(
+        onNavigateUpClick = dropUnlessResumed(block = navController::navigateUp),
+    )
+}
+
+@Composable
+private fun RoborazziScreen(
+    onNavigateUpClick: () -> Unit,
+) {
     Document(
         title = stringResource(id = R.string.feature_roborazzi_title),
         markdown = "TODO",
+        onNavigateUpClick = onNavigateUpClick,
     )
 }
 
@@ -17,5 +30,7 @@ internal fun RoborazziScreen() {
 @Composable
 private fun RoborazziScreenPreview() {
     // TODO: Theme
-    RoborazziScreen()
+    RoborazziScreen(
+        onNavigateUpClick = {},
+    )
 }
