a4e04c92879bbf9797c6f6e951c2300efb9e1132
Daiji256 <daiji.szk@gmail.com>
2025-03-07 19:59:17 +0900

    delete suppress

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
@@ -25,5 +23,3 @@ allprojects {
         }
     }
 }
-
-true // Needed to make the Suppress annotation work for the plugins block
