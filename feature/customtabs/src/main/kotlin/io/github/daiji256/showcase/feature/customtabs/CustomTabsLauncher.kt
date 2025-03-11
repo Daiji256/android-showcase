c7d57bcdbb2a46146751eafab032a7f43cddb9ee
Daiji256 <daiji.szk@gmail.com>
2025-03-11 22:25:19 +0900

    use findActivity

--- feature/customtabs/src/main/kotlin/io/github/daiji256/showcase/feature/customtabs/CustomTabsLauncher.kt
+++ feature/customtabs/src/main/kotlin/io/github/daiji256/showcase/feature/customtabs/CustomTabsLauncher.kt
@@ -3,10 +3,10 @@ package io.github.daiji256.showcase.feature.customtabs
 import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
-import androidx.activity.compose.LocalActivity
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -26,20 +26,28 @@ interface CustomTabsLauncher {
 
 private class CustomTabsLauncherImpl(val context: Context) : CustomTabsLauncher {
     override fun launch(uri: Uri) {
+        val activity = context.findActivity()
         try {
             CustomTabsIntent.Builder().build()
                 .apply {
-                    if (context !is Activity) {
+                    if (activity == null) {
                         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                     }
                 }
-                .launchUrl(context, uri)
+                .launchUrl(activity ?: context, uri)
         } catch (e: ActivityNotFoundException) {
             logger.error(e) { "Can't open $uri." }
-            Toast.makeText(context, "Can't open $uri.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity ?: context, "Can't open $uri.", Toast.LENGTH_SHORT).show()
         }
     }
 }
+
+private fun Context.findActivity(): Activity? =
+    when (this) {
+        is Activity -> this
+        is ContextWrapper -> baseContext.findActivity()
+        else -> null
+    }
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
@@ -51,6 +59,6 @@ internal object CustomTabsLauncherModule {
 
 @Composable
 fun rememberCustomTabsLauncher(): CustomTabsLauncher {
-    val context = LocalActivity.current ?: LocalContext.current
+    val context = LocalContext.current
     return remember(context) { CustomTabsLauncherImpl(context = context) }
 }
