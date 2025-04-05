e708fe14e9fb7464a225b8cc1607132b1c504e8f
Daiji256 <daiji.szk@gmail.com>
2025-04-05 20:00:27 +0900

    add val Project.android

--- build-logic/src/main/kotlin/io/github/daiji256/showcase/buildlogic/dsl/AndroidGradle.kt
+++ build-logic/src/main/kotlin/io/github/daiji256/showcase/buildlogic/dsl/AndroidGradle.kt
@@ -5,9 +5,13 @@ import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
 
 internal fun Project.android(action: BaseExtension.() -> Unit) =
     extensions.configure(action)
+
+internal val Project.android: BaseExtension
+    get() = extensions.getByType<BaseExtension>()
 
 internal fun Project.androidApplication(action: BaseAppModuleExtension.() -> Unit) =
     extensions.configure(action)
