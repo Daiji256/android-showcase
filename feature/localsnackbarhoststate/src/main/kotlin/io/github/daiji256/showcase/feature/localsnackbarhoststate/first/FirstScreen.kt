7f6eb27e86c756312ee605615a0d123241a42893
Daiji256 <daiji.szk@gmail.com>
2025-06-24 21:55:48 +0900

    add document

--- feature/localsnackbarhoststate/src/main/kotlin/io/github/daiji256/showcase/feature/localsnackbarhoststate/first/FirstScreen.kt
+++ feature/localsnackbarhoststate/src/main/kotlin/io/github/daiji256/showcase/feature/localsnackbarhoststate/first/FirstScreen.kt
@@ -1,14 +1,18 @@
 package io.github.daiji256.showcase.feature.localsnackbarhoststate.first
 
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.dropUnlessResumed
 import io.github.daiji256.showcase.core.ui.document.Document
 import io.github.daiji256.showcase.core.ui.localsnackbarhoststate.LocalSnackbarHostState
 import io.github.daiji256.showcase.core.ui.markdown.Markdown
+import io.github.daiji256.showcase.feature.localsnackbarhoststate.R
 import kotlinx.coroutines.launch
 
 @Composable
@@ -17,36 +21,46 @@ internal fun FirstScreen(
     onNavigateToSecondScreenClick: () -> Unit,
 ) {
     Document(
-        title = "TODO",
+        title = stringResource(id = R.string.feature_lshs_first_title),
         onNavigateUpClick = onNavigateUpClick,
     ) {
         val snackbarHostState = LocalSnackbarHostState.current
         val coroutineScope = rememberCoroutineScope()
+        val snackbarMessage = stringResource(id = R.string.feature_lshs_first_snackbar_message)
 
-        Markdown(
-            markdown = "TODO",
-        )
         Button(
             onClick = {
                 coroutineScope.launch {
                     snackbarHostState.showSnackbar(
-                        message = "TODO1",
+                        message = snackbarMessage,
                         duration = SnackbarDuration.Long,
                     )
                 }
             },
         ) {
-            Markdown(
-                markdown = "TODO1",
+            Text(
+                text = stringResource(
+                    id = R.string.feature_lshs_first_document_show_snackbar_button,
+                ),
             )
         }
         Button(
             onClick = dropUnlessResumed { onNavigateToSecondScreenClick() },
         ) {
-            Markdown(
-                markdown = "TODO",
+            Text(
+                text = stringResource(
+                    id = R.string.feature_lshs_first_navigate_to_second_screen_button,
+                ),
             )
         }
+
+        HorizontalDivider()
+
+        Markdown(
+            markdown = stringResource(
+                id = R.string.feature_lshs_first_document_summary_md,
+            ),
+        )
     }
 }
 
--- feature/localsnackbarhoststate/src/main/kotlin/io/github/daiji256/showcase/feature/localsnackbarhoststate/second/SecondScreen.kt
+++ feature/localsnackbarhoststate/src/main/kotlin/io/github/daiji256/showcase/feature/localsnackbarhoststate/second/SecondScreen.kt
@@ -2,12 +2,14 @@ package io.github.daiji256.showcase.feature.localsnackbarhoststate.second
 
 import androidx.compose.material3.Button
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.daiji256.showcase.core.ui.document.Document
 import io.github.daiji256.showcase.core.ui.localsnackbarhoststate.LocalSnackbarHostState
-import io.github.daiji256.showcase.core.ui.markdown.Markdown
+import io.github.daiji256.showcase.feature.localsnackbarhoststate.R
 import kotlinx.coroutines.launch
 
 @Composable
@@ -15,27 +17,27 @@ internal fun SecondScreen(
     onNavigateUpClick: () -> Unit,
 ) {
     Document(
-        title = "TODO",
+        title = stringResource(id = R.string.feature_lshs_second_title),
         onNavigateUpClick = onNavigateUpClick,
     ) {
         val snackbarHostState = LocalSnackbarHostState.current
         val coroutineScope = rememberCoroutineScope()
+        val snackbarMessage = stringResource(id = R.string.feature_lshs_second_snackbar_message)
 
-        Markdown(
-            markdown = "TODO",
-        )
         Button(
             onClick = {
                 coroutineScope.launch {
                     snackbarHostState.showSnackbar(
-                        message = "TODO2",
+                        message = snackbarMessage,
                         duration = SnackbarDuration.Long,
                     )
                 }
             },
         ) {
-            Markdown(
-                markdown = "TODO2",
+            Text(
+                text = stringResource(
+                    id = R.string.feature_lshs_second_document_show_snackbar_button,
+                ),
             )
         }
     }
