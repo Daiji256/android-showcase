964480195aa4090ca84deb725dd3b3912d1f21bd
Daiji256 <daiji.szk@gmail.com>
2025-06-24 12:32:37 +0900

    add demo

--- feature/localsnackbarhoststate/src/main/kotlin/io/github/daiji256/showcase/feature/localsnackbarhoststate/first/FirstScreen.kt
+++ feature/localsnackbarhoststate/src/main/kotlin/io/github/daiji256/showcase/feature/localsnackbarhoststate/first/FirstScreen.kt
@@ -1,11 +1,15 @@
 package io.github.daiji256.showcase.feature.localsnackbarhoststate.first
 
 import androidx.compose.material3.Button
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.dropUnlessResumed
 import io.github.daiji256.showcase.core.ui.document.Document
+import io.github.daiji256.showcase.core.ui.localsnackbarhoststate.LocalSnackbarHostState
 import io.github.daiji256.showcase.core.ui.markdown.Markdown
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun FirstScreen(
@@ -16,9 +20,26 @@ internal fun FirstScreen(
         title = "TODO",
         onNavigateUpClick = onNavigateUpClick,
     ) {
+        val snackbarHostState = LocalSnackbarHostState.current
+        val coroutineScope = rememberCoroutineScope()
+
         Markdown(
             markdown = "TODO",
         )
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = "TODO1",
+                        duration = SnackbarDuration.Long,
+                    )
+                }
+            },
+        ) {
+            Markdown(
+                markdown = "TODO1",
+            )
+        }
         Button(
             onClick = dropUnlessResumed { onNavigateToSecondScreenClick() },
         ) {
--- feature/localsnackbarhoststate/src/main/kotlin/io/github/daiji256/showcase/feature/localsnackbarhoststate/second/SecondScreen.kt
+++ feature/localsnackbarhoststate/src/main/kotlin/io/github/daiji256/showcase/feature/localsnackbarhoststate/second/SecondScreen.kt
@@ -1,9 +1,14 @@
 package io.github.daiji256.showcase.feature.localsnackbarhoststate.second
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.daiji256.showcase.core.ui.document.Document
+import io.github.daiji256.showcase.core.ui.localsnackbarhoststate.LocalSnackbarHostState
 import io.github.daiji256.showcase.core.ui.markdown.Markdown
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun SecondScreen(
@@ -13,9 +18,26 @@ internal fun SecondScreen(
         title = "TODO",
         onNavigateUpClick = onNavigateUpClick,
     ) {
+        val snackbarHostState = LocalSnackbarHostState.current
+        val coroutineScope = rememberCoroutineScope()
+
         Markdown(
             markdown = "TODO",
         )
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = "TODO2",
+                        duration = SnackbarDuration.Long,
+                    )
+                }
+            },
+        ) {
+            Markdown(
+                markdown = "TODO2",
+            )
+        }
     }
 }
 
