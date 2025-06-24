952f8f362648053c5799ddfed62a0753eb417deb
Daiji256 <daiji.szk@gmail.com>
2025-06-24 12:31:09 +0900

    add LocalSnackbarHostState

---
+++ core/ui/src/main/kotlin/io/github/daiji256/showcase/core/ui/localsnackbarhoststate/LocalSnackbarHostState.kt
@@ -0,0 +1,6 @@
+package io.github.daiji256.showcase.core.ui.localsnackbarhoststate
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.compositionLocalOf
+
+val LocalSnackbarHostState = compositionLocalOf { SnackbarHostState() }
