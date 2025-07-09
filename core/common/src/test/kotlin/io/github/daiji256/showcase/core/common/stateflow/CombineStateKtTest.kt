1aa80d5742831848cdda0d2452677c8cf5aa8035
Daiji256 <daiji.szk@gmail.com>
2025-07-09 20:26:34 +0900

    failure test

--- core/common/src/test/kotlin/io/github/daiji256/showcase/core/common/stateflow/CombineStateKtTest.kt
+++ core/common/src/test/kotlin/io/github/daiji256/showcase/core/common/stateflow/CombineStateKtTest.kt
@@ -16,7 +16,7 @@ class CombineStateKtTest {
             flow1,
             flow2,
         ) { value1, value2 ->
-            "$value1, $value2"
+            ""
         }
 
         assertEquals(target.value, "1, 2.0")
