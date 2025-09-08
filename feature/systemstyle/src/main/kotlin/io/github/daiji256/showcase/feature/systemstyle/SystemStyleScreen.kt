f7a9a1b07c98bdddbb4c5c79c316d221b14a7b52
Daiji256 <daiji.szk@gmail.com>
2025-09-09 00:04:11 +0900

    Docs: Add SystemStyleScreen description
    Add a description to the SystemStyleScreen explaining that it retrieves theme and other settings specified in the System.

--- feature/systemstyle/src/main/kotlin/io/github/daiji256/showcase/feature/systemstyle/SystemStyleScreen.kt
+++ feature/systemstyle/src/main/kotlin/io/github/daiji256/showcase/feature/systemstyle/SystemStyleScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.daiji256.showcase.core.designsystem.theme.ShowcaseTheme
 import io.github.daiji256.showcase.core.ui.document.Document
+import io.github.daiji256.showcase.core.ui.markdown.Markdown
 
 @Composable
 internal fun SystemStyleScreen(
@@ -14,6 +15,9 @@ internal fun SystemStyleScreen(
         title = stringResource(id = R.string.feature_system_style_title),
         onNavigateUpClick = onNavigateUpClick,
     ) {
+        Markdown(
+            markdown = stringResource(id = R.string.feature_system_style_document_md),
+        )
         SystemStyleValues()
     }
 }
