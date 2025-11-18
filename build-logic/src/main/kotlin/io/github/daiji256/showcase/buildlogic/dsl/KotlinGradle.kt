018ad1639f72d42b3f3d869a68db8acfb29ea540
Daiji256 <daiji.szk@gmail.com>
2025-11-18 12:54:25 +0900

    Move ksp extension function

--- build-logic/src/main/kotlin/io/github/daiji256/showcase/buildlogic/dsl/KotlinGradle.kt
+++
@@ -1,7 +0,0 @@
-package io.github.daiji256.showcase.buildlogic.dsl
-
-import org.gradle.api.artifacts.MinimalExternalModuleDependency
-import org.gradle.kotlin.dsl.DependencyHandlerScope
-
-internal fun DependencyHandlerScope.ksp(artifact: MinimalExternalModuleDependency) =
-    add("ksp", artifact)
