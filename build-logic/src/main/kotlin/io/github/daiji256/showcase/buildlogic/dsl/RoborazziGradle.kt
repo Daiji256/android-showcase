fce788a35f2d58a2e15d3184cdaba8a9bc1099a4
Daiji256 <daiji.szk@gmail.com>
2025-11-18 12:53:31 +0900

    Remove Gradle DSL helper files

--- build-logic/src/main/kotlin/io/github/daiji256/showcase/buildlogic/dsl/RoborazziGradle.kt
+++
@@ -1,8 +0,0 @@
-package io.github.daiji256.showcase.buildlogic.dsl
-
-import io.github.takahirom.roborazzi.RoborazziExtension
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-
-internal fun Project.roborazzi(action: RoborazziExtension.() -> Unit) =
-    extensions.configure<RoborazziExtension>(action)
