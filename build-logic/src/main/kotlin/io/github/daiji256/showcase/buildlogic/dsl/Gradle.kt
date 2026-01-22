db8d6f7f8a8aa6dc70c4811e3e314255c0715328
Daiji256 <daiji.szk@gmail.com>
2026-01-23 00:18:55 +0900

    refactor: update implementation functions to use Dependency type

--- build-logic/src/main/kotlin/io/github/daiji256/showcase/buildlogic/dsl/Gradle.kt
+++ build-logic/src/main/kotlin/io/github/daiji256/showcase/buildlogic/dsl/Gradle.kt
@@ -1,23 +1,16 @@
 package io.github.daiji256.showcase.buildlogic.dsl
 
 import org.gradle.api.artifacts.Dependency
-import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 
 internal fun DependencyHandlerScope.implementation(artifact: Dependency) =
     add("implementation", artifact)
 
-internal fun DependencyHandlerScope.implementation(artifact: MinimalExternalModuleDependency) =
-    add("implementation", artifact)
-
-internal fun DependencyHandlerScope.debugImplementation(artifact: MinimalExternalModuleDependency) =
+internal fun DependencyHandlerScope.debugImplementation(artifact: Dependency) =
     add("debugImplementation", artifact)
 
 internal fun DependencyHandlerScope.testImplementation(artifact: Dependency) =
     add("testImplementation", artifact)
 
-internal fun DependencyHandlerScope.testImplementation(artifact: MinimalExternalModuleDependency) =
-    add("testImplementation", artifact)
-
-internal fun DependencyHandlerScope.ksp(artifact: MinimalExternalModuleDependency) =
+internal fun DependencyHandlerScope.ksp(artifact: Dependency) =
     add("ksp", artifact)
