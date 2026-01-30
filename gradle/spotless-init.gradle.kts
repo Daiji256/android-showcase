fb87f39e0ae7dc4ae15334af4d5a7d734f1ee219
Daiji256 <daiji.szk@gmail.com>
2026-01-30 12:04:49 +0900

    chore: change subprojects to allprojects in spotless-init.gradle.kts

--- gradle/spotless-init.gradle.kts
+++ gradle/spotless-init.gradle.kts
@@ -12,37 +12,41 @@ initscript {
 }
 
 rootProject {
-    allprojects {
-        // renovate: datasource=maven depName=com.pinterest.ktlint:ktlint-cli
-        val ktlintVersion = "1.8.0"
+    // renovate: datasource=maven depName=com.pinterest.ktlint:ktlint-cli
+    val ktlintVersion = "1.8.0"
 
-        // renovate: datasource=maven depName=io.nlopez.compose.rules:ktlint
-        val ktlintComposeRulesVersion = "0.5.6"
+    // renovate: datasource=maven depName=io.nlopez.compose.rules:ktlint
+    val ktlintComposeRulesVersion = "0.5.6"
 
-        apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
-        extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-            kotlin {
-                target("**/*.kt")
-                targetExclude("**/build/**/*.kt")
-                ktlint(ktlintVersion)
-                    .customRuleSets(
-                        listOf(
-                            "io.nlopez.compose.rules:ktlint:$ktlintComposeRulesVersion",
-                        ),
-                    )
-                    .setEditorConfigPath(rootProject.file(".editorconfig"))
-                    .editorConfigOverride(
-                        mapOf(
-                            "compose_allowed_composition_locals" to "LocalSnackbarHostState",
-                        ),
-                    )
-            }
+    val editorConfigOverride = mapOf(
+        "ktlint_code_style" to "android_studio",
+        "ktlint_experimental" to "enabled",
+        "ktlint_function_naming_ignore_when_annotated_with" to "Composable,Test",
+        "ktlint_standard_class-signature" to "disabled",
+        "ktlint_standard_function-signature" to "disabled",
+        "ktlint_standard_property-naming" to "disabled",
+        "compose_allowed_composition_locals" to "LocalSnackbarHostState",
+    )
 
-            kotlinGradle {
-                target("**/*.kts")
-                targetExclude("**/build/**/*.kts")
-                ktlint(ktlintVersion)
-            }
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+    extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("**/build/**/*.kt")
+            ktlint(ktlintVersion)
+                .editorConfigOverride(editorConfigOverride)
+                .customRuleSets(
+                    listOf(
+                        "io.nlopez.compose.rules:ktlint:$ktlintComposeRulesVersion",
+                    ),
+                )
+        }
+
+        kotlinGradle {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            ktlint(ktlintVersion)
+                .editorConfigOverride(editorConfigOverride)
         }
     }
 }
