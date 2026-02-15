c5293d987e96ef996e6ae215d0b7a067b7628293
Daiji256 <daiji.szk@gmail.com>
2026-02-15 10:41:33 +0900

    feat: add rememberNavTree composable for navigation tree management

---
+++ core/ui/src/main/kotlin/io/github/daiji256/showcase/core/ui/navigation/RememberNavTree.kt
@@ -0,0 +1,21 @@
+package io.github.daiji256.showcase.core.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation3.runtime.NavKey
+
+/**
+ * Remembers a navigation tree ([NavNode.Stack]) for Compose.
+ *
+ * @param T the type of the tree
+ * @param initial the initial children of the tree
+ * @return a remembered [NavNode.Stack]
+ */
+@Composable
+fun <T : NavKey> rememberNavTree(
+    vararg initial: NavNode<T>,
+): NavNode.Stack<T> =
+    // TODO: Saveable
+    remember {
+        NavNode.Stack(*initial)
+    }
