516ebf5f60f070d2b6c72937c0d81421ec0914c7
Daiji256 <daiji.szk@gmail.com>
2026-05-23 14:20:44 +0900

    fix(ui): add explicit Shape type to LocalWindowShape

--- core/ui/src/main/kotlin/io/github/daiji256/showcase/core/ui/window/LocalWindowShape.kt
+++ core/ui/src/main/kotlin/io/github/daiji256/showcase/core/ui/window/LocalWindowShape.kt
@@ -3,11 +3,12 @@ package io.github.daiji256.showcase.core.ui.window
 import android.view.RoundedCorner
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.compositionLocalWithComputedDefaultOf
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.LayoutDirection
 
-val LocalWindowShape = compositionLocalWithComputedDefaultOf {
+val LocalWindowShape = compositionLocalWithComputedDefaultOf<Shape> {
     val view = LocalView.currentValue
     val insets = view.rootWindowInsets
     val topLeft = insets?.getRoundedCorner(RoundedCorner.POSITION_TOP_LEFT)?.radius ?: 0
